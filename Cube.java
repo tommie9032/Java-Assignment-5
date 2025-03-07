b0cb3a2b980abe4756da7d3a2d4af732e7ad6c3b
tommie9032 <tomthomas9032@gmail.com>
2025-03-07 20:39:47 +0530

    Added Cube.java

---
+++ Cube.java
@@ -0,0 +1,22 @@
+import java.lang.*;
+class Cube extends Shape{
+	public Cube(double side){
+
+		super(side,0,0,6);
+
+	}
+
+
+	public double calculateArea(){
+		return 6 * dim_one * dim_one;
+
+	}
+
+	public double calculatePerimeter(){
+		return 12 * dim_one;
+	}
+
+	public double calculateVolume(){
+		return dim_one * dim_one * dim_one;
+	}
+}
