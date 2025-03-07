825c6a6d253102f1ea46599cb45e81558fcc8d87
tommie9032 <tomthomas9032@gmail.com>
2025-03-07 20:41:00 +0530

    Added Cylinder.java

---
+++ Cylinder.java
@@ -0,0 +1,19 @@
+//Cylinder.java
+import java.lang.*;
+class Cylinder extends Shape{
+	public Cylinder(double radius , double height){
+		super(radius,height,0,0);
+	}
+
+	public double calculateArea(){
+		return (2 * 3.14 * dim_one) * (dim_one + dim_two);
+	}
+
+	public double calculatePerimeter(){
+		return 2 * (( 2 * dim_one ) + dim_two );
+	}
+
+	public double calculateVolume(){
+		return 3.14 * dim_one * dim_one * dim_two; 
+	}
+}
